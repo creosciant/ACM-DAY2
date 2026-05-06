723bbf18df2add3480470b4948654898f02a9d17
creosciant <lastnameispiccolo@gmail.com>
2026-05-06 13:31:16 +0000

    Version 1.0.0.2

--- config/settings.py
+++ config/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-zart4wx%n@n%o-%fr-+lsdo=w7_hy6!r#f3daz@dq+0#cwq&^5"
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-zart4wx%n@n%o-%fr-+lsdo=w7_hy6!r#f3daz@dq+0#cwq&^5')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
 ALLOWED_HOSTS = ['*']
 
@@ -75,12 +76,17 @@ WSGI_APPLICATION = "config.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+# Use SQLite in development, but skip database operations in Vercel builds
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
+
+# Disable database during collectstatic
+if os.environ.get('VERCEL'):
+    DATABASES = {}
 
 
 # Password validation
